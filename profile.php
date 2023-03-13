a85b602daa6cb70842a79028959c487d541e4e7c
1063451124 <1063451124@qq.com>
2023-03-13 16:11:34 +0800

    demoo

---
+++ profile.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.101.0">
+  <title>CityU Career Coach appointment</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/navbar-fixed/">
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+
+
+  <!-- Custom styles for this template -->
+  <link href="../assets/dist/css/navbar-top-fixed.css" rel="stylesheet">
+  <link href="../assets/dist/css/cca.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <?php
+  require "./navi.php";
+  ?>
+
+  <main role="main" class="container align-middle">
+
+    <form action="http://www.abc.com/" method="get">
+      <div class="form-group">
+        <label for="formGroupExampleInput" class="control-label" >First Name </label>
+        <input type="text" class="form-control" id="#" placeholder="" required>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="##" aria-describedby="emailHelp"
+          placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlFile1">Update your CV</label>
+        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+      </div>
+      <!--
+      <fieldset>
+        <legend>Login information</legend>
+        User Name: <input name="N" type="text"> <br><br>
+
+        Password: <input name="P" type="password"> <br><br>
+      </fieldset>
+
+      <input type="radio" name="G" value="M" checked> Male
+      <input type="radio" name="G" value="F"> Female <br><br>
+
+      <input type="checkbox" name="M" value="Y" checked> Lifetime membership <br><br>
+
+      Job: <br>
+      <select name="J">
+        <option value="1">Student</option>
+        <option value="2">Engineer</option>
+        <option value="3" selected>Self Employed</option>
+      </select>
+
+      <br><br>Special Request: <br>
+      <textarea id="R" name="Request" rows="5" cols="80"></textarea>
+
+      <br><input type="submit" value="Go">
+
+      <input type="reset">
+      <input type="button" value="Click Me!" onclick="alert('Good Evening!');">
+      <button><img src="https://www.cs.cityu.edu.hk/~kclee/pic.jpg" width=30></button>
+      -->
+    </form>
+
+  </main>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
---
+++ schedule.php
@@ -0,0 +1,22 @@
+<table class="table col-md-8 table-bordered" id="schedule">
+        <caption id="tbs">Time Slots</caption>
+        <tr>
+          <td>Amy Leung</td>
+          <td>09:30 - 10:15</td>
+          <td>10:15 - 11:00</td>
+          <td>11:00 - 11:45</td>
+        </tr>
+        <tr>
+          <td>Steve Man</td>
+          <td>11:45 - 12:30</td>
+          <td>12:30 - 13:15</td>
+          <td>14:00 - 14:45</td>
+          <td>14:45 - 15:30</td>
+        </tr>
+        <tr>
+          <td>Winnie Lee</td>
+          <td>15:30 - 16:15</td>
+          <td>16:15 - 17:00</td>
+          <td>17:00 - 17:45</td>
+        </tr>
+      </table>
